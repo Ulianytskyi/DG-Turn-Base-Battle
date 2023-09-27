c5ea6300c0415ff1c81687328d7c5426149e6c55
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-09-27 17:59:30 +0300

    Commit 3

--- script2.js
+++ script2.js
@@ -1,11 +1,22 @@
 const gameBoard = document.getElementById('board');
+const gameField = document.getElementById('game-field');
+
+// terrains -------------------------------------------------------
 
 function generatePositions(value) {
     let positions = [];
     for (let i = 0; i < value; i++) {
         const pos = document.createElement('div');
         pos.className = "pos pos" + (i + 1);
-        pos.innerText = (i + 1);
+
+        const pos2 = document.createElement('div');
+        pos2.className = "sprite-container2";
+
+        const pos3 = document.createElement('div');
+        pos3.className = "sprite terrain-sprites terrain";
+        
+        pos2.appendChild(pos3);
+        pos.appendChild(pos2);
         positions.push(pos);
     }
     return positions;
@@ -14,24 +25,29 @@ function generatePositions(value) {
 const newPos = generatePositions(12);
 newPos.forEach(pos => gameBoard.appendChild(pos));
 
-function createUnits(value) {
+// units ------------------------------------------------------------
+
+function createUnits(value, type) {
     let units = [];
     for (let i = 0; i < value; i++) {
-        const unit = document.createElement('div');
-        const sword = document.createElement('div');
-        sword.innerHTML = '🗡';
-        sword.className = 'sword sword' + (i+1);
-        unit.innerHTML = '🧍🏻‍♂️';
-        unit.className = 'unit unit' + (i+1);
-        units.push(unit, sword);
+        const unit1 = document.createElement('div');
+        unit1.className = `pixel pos-${type}` + (i+1);
+
+        const unit2 = document.createElement('div');
+        unit2.className = `sprite-container`;
+
+        const unit3 = document.createElement('div');
+        unit3.className = `sprite unit-sprites ${type}`;
+
+        unit2.appendChild(unit3);
+        unit1.appendChild(unit2);
+        units.push(unit1);
     }
     return units;
 }
 
-const newUnits = createUnits(6);
-newUnits.forEach(unit => gameBoard.appendChild(unit));
+const heroUnits = createUnits(6, 'hero');
+const enemyUnits = createUnits(3, 'enemy');
+heroUnits.forEach(unit => gameField.appendChild(unit));
+enemyUnits.forEach(unit => gameField.appendChild(unit));
 
-const enemy = document.createElement('div');
-enemy.innerHTML = '🦄';
-enemy.className = 'enemy';
-gameBoard.appendChild(enemy);
