f915b0a59dd4ec221f84f06a7061ec4ea0dae25c
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-09-28 23:44:16 +0300

    Commit 5

--- script3.js
+++ script3.js
@@ -1,57 +1,143 @@
 const gameField = document.getElementById('game-field');
-const gameBoard = document.getElementById('board');
 
-function createTerrain(value) {
-    let bgTerrains = [];
-    for (let i = 0; i < value; i++) {
-        const bg1 = document.createElement('div');
-        bg1.className = `terrain-sprite-container pos-ter0`;
-
-        const bg2 = document.createElement('div');
-        bg2.className = `terrain-sprite`;
-
-        bg1.appendChild(bg2);
-        bgTerrains.push(bg1);
-    }
-    return bgTerrains;
-}
-
-const terrains = createTerrain(10);
-terrains.forEach(terrain => gameBoard.appendChild(terrain));
+// unit generation -------------------------------------------------------
 
 function createUnits(value, type) {
     let units = [];
     for (let i = 0; i < value; i++) {
         const unit1 = document.createElement('div');
-        unit1.className = `sprite-container pos-${type+(i+1)}`;
+        unit1.className = `sprite ${type} pos-${type+(i+1)}`;
 
-        const unit2 = document.createElement('div');
-        unit2.className = `sprite ${type}`;
-
-        unit1.appendChild(unit2);
         units.push(unit1);
     }
     return units;
 }
 
-const heroUnits = createUnits(6, 'hero');
+const heroUnits = createUnits(9, 'hero');
 heroUnits.forEach(unit => gameField.appendChild(unit));
-
 
 const enemyUnits = createUnits(1, 'enemy');
 enemyUnits.forEach(unit => gameField.appendChild(unit));
 
 
+// unit animation ----------------------------------------------------------
 let i = 1;
 function heroIdle() {
     // heroUnits[0].lastChild.className = `sprite hero idle${i+1}`;
 
     // OR
 
-    // heroUnits.forEach(element => {
-    //     element.lastChild.className = `sprite hero idle${i}`;
-    // })
+    heroUnits.forEach(element => {
+        element.lastChild.className = `sprite hero idle${i}`;
+    })
 
     i = (i + 1) % 2; 
 }
-setInterval(heroIdle, 1500);
+// setInterval(heroIdle, 1500);
+
+// game board ----------------------------------------------------------
+
+const gameBoard1 = document.getElementById('game-board1');
+const gameBoard2 = document.getElementById('game-board2');
+
+for (let i = 0; i < 225; i++) {
+    const square = document.createElement('div');
+    square.classList.add('terrain-sprite');
+    gameBoard1.appendChild(square);
+}
+
+for (let j = 0; j < 225; j++) {
+    const square = document.createElement('div');
+    square.classList.add('terrain-sprite');
+    gameBoard2.appendChild(square);
+}
+
+// mouse events ----------------------------------------------------------
+
+const targetElement = document.getElementById('game-container');
+
+targetElement.addEventListener('mousedown', handleMouseDown);
+
+
+function handleMouseDown(event) {
+    const rect = targetElement.getBoundingClientRect();
+    
+    const xRelativeToElement = event.clientX - rect.left;
+    const yRelativeToElement = event.clientY - rect.top;
+
+    // console.log(`Координати мишки: X=${xRelativeToElement}, Y=${yRelativeToElement}`);
+}
+
+function checkObject(x, y) {
+
+}
+
+gameField.addEventListener('mousedown', handleClick);
+
+function handleClick(event) {
+    
+    const clickedElement = event.target;
+
+    
+    if (clickedElement.classList.contains('sprite')) {
+        if (clickedElement.classList.contains('hero')) {
+            
+            console.log('Клікнуто на героя:', clickedElement.classList);
+
+        } else if (clickedElement.classList.contains('enemy')) {
+            
+            console.log('Клікнуто на ворога:', clickedElement.classList);
+        }
+    }
+}
+
+// battle ---------------------
+
+const heroes = document.querySelectorAll('.hero');
+const enemies = document.querySelectorAll('.enemy');
+
+let selectedHero = null;
+let selectedEnemy = null;
+
+heroes.forEach(hero => {
+    hero.addEventListener('click', handleHeroClick);
+});
+
+enemies.forEach(enemy => {
+    enemy.addEventListener('click', handleEnemyClick);
+});
+
+function handleHeroClick(event) {
+    if (selectedHero === null) {
+        selectedHero = event.target;
+        selectedHero.classList.add('selected');
+    } else if (selectedHero === event.target) {
+        selectedHero.classList.remove('selected');
+        selectedHero = null;
+    } else {
+        selectedHero.classList.remove('selected');
+        selectedHero = event.target;
+        selectedHero.classList.add('selected');
+    }
+}
+
+function handleEnemyClick(event) {
+    if (selectedHero !== null) {
+        selectedEnemy = event.target;
+        selectedEnemy.classList.add('selected');
+        console.log('Двобій');
+    }
+}
+
+document.getElementById('game-field').addEventListener('click', clearSelection);
+
+function clearSelection() {
+    if (selectedHero !== null) {
+        selectedHero.classList.remove('selected');
+        selectedHero = null;
+    }
+    if (selectedEnemy !== null) {
+        selectedEnemy.classList.remove('selected');
+        selectedEnemy = null;
+    }
+}
